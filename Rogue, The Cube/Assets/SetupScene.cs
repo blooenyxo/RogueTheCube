bdcef8a033fe53d598d36f83062187cd8fb137de
blooenyxo <blooenyxo@gmail.com>
2020-03-06 22:56:16 +0200

    Setup Scene
    Continue Game and Reset Progress added

--- Rogue, The Cube/Assets/SetupScene.cs
+++ Rogue, The Cube/Assets/SetupScene.cs
@@ -7,17 +7,37 @@ public class SetupScene : MonoBehaviour
 
     public Dropdown classSelection;
     public Dropdown weaponSelection;
+    public Button continueButton;
+
+    private void Start()
+    {
+        if (PlayerPrefs.GetInt("continue", 0) == 1)
+        {
+            continueButton.interactable = true;
+            //continueButton.colors = Color.black;
+        }
+        else
+            continueButton.interactable = false;
+    }
 
     public void StartGame()
     {
         PlayerPrefs.SetInt("playerClass", classSelection.value);
         PlayerPrefs.SetInt("playerStartingWeapon", weaponSelection.value);
+        PlayerPrefs.SetInt("continue", 1);
 
         SceneManager.LoadScene("MainScene");
     }
 
     public void ContinueGame()
     {
-        SceneManager.LoadScene("MainScene");
+        if (PlayerPrefs.GetInt("continue", 0) == 1)
+            SceneManager.LoadScene("MainScene");
+    }
+
+    public void ResetGameProgress()
+    {
+        continueButton.interactable = false;
+        PlayerPrefs.SetInt("continue", 0);
     }
 }
